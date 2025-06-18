1fab621af7a43176886c9e08a2da9b506eb48009
Ovann <hiuraksasa1927@gmail.com>
2025-06-18 14:22:54 +0700

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,15 +1,1 @@
-const cursor = document.querySelector(".cursor");
 
-document.addEventListener("mousemove", (e) => {
-  cursor.style.left = e.clientX + "px";
-  cursor.style.top = e.clientY + "px";
-});
-
-document.addEventListener("click", (e) => {
-  const ripple = document.createElement("span");
-  ripple.className = "ripple";
-  ripple.style.left = e.clientX + "px";
-  ripple.style.top = e.clientY + "px";
-  document.body.appendChild(ripple);
-  setTimeout(() => ripple.remove(), 500);
-});
